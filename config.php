0280329aba0db7bbfadd589876a212bb662751e7
Claudio Fior <caiofior@gmail.com>
2013-01-10 16:44:44 +0100

    Use of Zend Db for database connection

--- config.php
+++ config.php
@@ -11,4 +11,12 @@ $ZEND_PATH ='/usr/share/php/libzend-framework-php';
 /**
  * Base url of the web site
  */
-$BASE_URL ='http://127.0.0.1/progettobosco/';
+$BASE_URL ='http://127.0.0.1/progettobosco/public/';
+$DB_CONFIG = array(
+    'adapter'   => 'Pgsql', 
+    'host'      => 'localhost', 
+    'port'      => '5432', 
+    'username'  => 'postgres', 
+    'password'  => 'postgres', 
+    'dbname'    => 'progettobosco15-05-11'
+    );
--- include/pageboot.php
+++ include/pageboot.php
@@ -6,15 +6,15 @@
  */
 require (__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config.php');
 if (isset($ZEND_PATH))
-    set_include_path(get_include_path().PATH_SEPARATOR.'/usr/share/php/libzend-framework-php');
+    set_include_path(get_include_path().PATH_SEPARATOR.$ZEND_PATH);
+set_include_path(get_include_path().PATH_SEPARATOR.__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib');
 require('Zend'.DIRECTORY_SEPARATOR.'Loader.php');
 require(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'pb'.DIRECTORY_SEPARATOR.'autoloader.php') ;
 require(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'pb_base'.DIRECTORY_SEPARATOR.'functions.php') ;
 require(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'pb_base'.DIRECTORY_SEPARATOR.'insert_update.php') ;
-
-
-$dbname="progettobosco15-05-11"; //progettobosco28-09-10 dumpSboarina  pietrabbondante
-$conn=pg_Connect( "host=localhost user=postgres password=postgres dbname=$dbname" ) or die("Impossibile collegarsi al server");
-if(!$conn){die("Impossibile connettersi al database $dbname");}
 Zend_Loader::loadClass('Zend_View');
+Zend_Loader::loadClass('Zend_Db');
+
+$db = Zend_Db::factory($DB_CONFIG['adapter'],$DB_CONFIG);
+$db->getConnection();
 
