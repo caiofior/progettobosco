5f318d071ca8248d74445b26e2586f209f61482b
Claudio Fior <caiofior@gmail.com>
2013-01-22 16:19:32 +0100

    Added user management to admin

---
+++ views/blocks/content/user_manage.php
@@ -0,0 +1,44 @@
+                        <!-- main -->
+			<div id="main">	
+				<div class="post">
+			
+					<h2>Utenti</h2>
+					<table cellpadding="0" cellspacing="0" border="0" class="display" id="user">
+                                                <thead>
+                                                        <tr>
+                                                                <th width="2%">Id</th>
+                                                                <th width="18%">Nome utente</th>
+                                                                <th width="10%">Nome</th>
+                                                                <th width="10%">Tel.</th>
+                                                                <th width="10%">Città</th>
+                                                                <th width="10%">Org.</th>
+                                                                <th width="10%">Reg. il</th>
+                                                                <th width="10%">Azioni</th>
+                                                        </tr>
+                                                </thead>
+                                                <tbody>
+                                                        <tr>
+                                                                <td colspan="5" class="dataTables_empty">Caricamento dei dati</td>
+                                                        </tr>
+                                                </tbody>
+                                                <tfoot>
+                                                        <tr>
+                                                                <th >Id</th>
+                                                                <th >Nome utente</th>
+                                                                <th >Nome</th>
+                                                                <th >Tel.</th>
+                                                                <th >Città</th>
+                                                                <th >Org.</th>
+                                                                <th >Reg. il</th>
+                                                                <th >Azioni</th>
+                                                        </tr>
+                                                </tfoot>
+                                        </table>
+                                        	
+				<!-- /post -->	
+				</div>	
+                            
+
+                            
+			<!-- /main -->	
+			</div>
