2ea01857327617f5d28a7e0bc72c250b09b12f0d
Claudio Fior <caiofior@gmail.com>
2013-03-08 17:10:25 +0100

    Shrub and herbaces for B2

---
+++ lib/pb/forest/attribute/corkherbaceuscompositioncoll.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Manages Herbaceus Composition collection
+ * 
+ * Manages Herbaceus Composition collection
+ * 
+ * @author Claudio Fior <caiofior@gmail.com>
+ * @copyright CRA
+ */
+namespace forest\attribute;
+if (!class_exists('Content')) {
+    $file = 'form'.DIRECTORY_SEPARATOR.array(basename(__FILE__));
+    $PHPUNIT=true;
+    require (__DIR__.
+                DIRECTORY_SEPARATOR.'..'.
+                DIRECTORY_SEPARATOR.'..'.
+                DIRECTORY_SEPARATOR.'..'.
+                DIRECTORY_SEPARATOR.'..'.
+                DIRECTORY_SEPARATOR.'include'.
+                DIRECTORY_SEPARATOR.'pageboot.php');
+}
+/**
+ * Manages Herbaceus Composition collection
+ * 
+ * Manages Herbaceus Composition collection
+ * 
+ * @author Claudio Fior <caiofior@gmail.com>
+ * @copyright CRA
+ */
+class HerbaceusCompositionColl  extends \ContentColl  {
+    /**
+     * Forest Reference
+     * @var \forest\form\B1
+     */
+    protected $form_b1=null;
+    /**
+     * Instantiates the table
+     */
+    public function __construct() {
+        parent::__construct(new HerbaceusComposition());
+    }
+    /**
+     * Sets the form reference
+     * @param \forest\form\B1 $form Form b1
+     */
+    public function setForm(\forest\form\B1 $form) {
+
+        $this->form_b1 = $form;
+    }
+     /**
+     * Customizes the select statement
+     * @param \Zend_Db_Select $select
+     * @param array $criteria Filtering criteria
+     * @return \Zend_Db_Select
+     */
+    protected function customSelect(\Zend_Db_Select $select,array $criteria ) {
+        $select->setIntegrityCheck(false)
+        ->from('erbacee',array(
+            '*',
+            'cod_colt_descriz'=>new \Zend_Db_Expr(
+                '( SELECT diz_erba.nome FROM diz_erba WHERE diz_erba.cod_coltu=erbacee.cod_coltu) '
+             )
+        ));
+        if ($this->form_b1 instanceof \forest\form\B1) {
+            $select->where(' cod_part = ? ',$this->form_b1->getData('cod_part'))
+            ->where(' proprieta = ? ',$this->form_b1->getData('proprieta'))
+            ->where(' cod_fo = ? ',$this->form_b1->getData('cod_fo'));
+            
+        }
+        $select->order('cod_coltu');
+        return $select;
+    }
+     /**
+     * Returns all contents without any filter
+     * @param array $criteria Filtering criteria
+     */
+    public function countAll(array $criteria = null) {
+        if ($this->form_b1 instanceof \forest\form\B1) {
+            $select = $this->content->getTable()->select()->from($this->content->getTable()->info('name'),'COUNT(*)');
+            $select->where(' cod_part = ? ',$this->form_b1->getData('cod_part'))
+            ->where(' proprieta = ? ',$this->form_b1->getData('proprieta'))
+            ->where(' cod_fo = ? ',$this->form_b1->getData('cod_fo'))
+            ;
+            return intval($this->content->getTable()->getAdapter()->fetchOne($select));
+        }
+        else 
+            parent::countAll();
+
+    }
+    /**
+     * Adds new forest composition 
+     * @return ForestCoverComposition
+     */
+    public function addItem() {
+        $forestcovercomposition = parent::addItem();
+        $forestcovercomposition->setData($this->form_b1->getData('cod_fo'),'cod_fo');
+        $forestcovercomposition->setData($this->form_b1->getData('cod_part'),'cod_part');
+        $forestcovercomposition->setData($this->form_b1->getData('proprieta'),'proprieta');
+        return $forestcovercomposition;
+    }
+ }
---
+++ views/blocks/content/schedab2/arbustive.php
@@ -0,0 +1,113 @@
+<div id="content_schedab_arbustive">
+    <?php
+    if (!isset($b1)) {
+        $b1 = new \forest\form\B1();
+        $b1->loadFromId($_REQUEST['id']);
+    }
+    $shrubcompositioncoll = $b1->getShrubCompositionColl();
+    
+    if (!key_exists('start', $_GET))
+            $_GET['start']=0;
+    $items_in_page =2;
+    
+    $shrubcompositioncoll->loadAll(array(
+        'start'=>$_GET['start'],
+        'length'=>$items_in_page
+    ));
+
+    foreach ($shrubcompositioncoll->getItems() as $shrubcomposition) :
+    ?>
+    <div>
+        <span>
+            <div>
+                <input readonly="readonly" id="cod_coltu_ar_descr_<?php echo $shrubcomposition->getData('objectid');?>" data-arbustive-id="<?php echo $shrubcomposition->getData('objectid');?>" name="cod_coltu_ar_descr_<?php echo $shrubcomposition->getData('objectid');?>" value="<?php echo $shrubcomposition->getRawData('cod_colt_descriz');?>" data-old-value="<?php echo $shrubcomposition->getRawData('cod_colt_descriz');?>">
+            </div>
+        </span>
+        <span>
+            <div>
+                <a href="<?php echo $GLOBALS['BASE_URL'];?>bosco.php?task=formb1&id=<?php echo $b1->getData('objectid');?>&deletearbustive=<?php echo $shrubcomposition->getData('objectid');?>"  >
+                    <img class="actions delete" src="images/empty.png" title="Cancella"/>
+                </a>
+            </div>
+        </span>
+    </div>
+    <?php endforeach; 
+    
+    $start = $_GET['start'];
+    unset($_GET['start']);
+    unset($_GET['task']);
+    unset($_GET['action']);
+    $baseurl = http_build_query($_GET);
+    $actions =array(
+        'first'=>array(
+            'url'=>'',
+            'data-update'=>''
+        ),
+        'prev'=>array(
+            'url'=>'',
+            'data-update'=>''
+        ),
+        'next'=>array(
+            'url'=>'',
+            'data-update'=>''
+        ),
+        'last'=>array(
+            'url'=>'',
+            'data-update'=>''
+        ),
+    );
+    $countall =$shrubcompositioncoll->countAll();
+    $last_page = floor($countall/$items_in_page)*$items_in_page;
+
+    if ($start>0) {
+        $actions['prev']=array(
+            'url'=>'href="?'.$baseurl.'&start='.max($start-$items_in_page,0).'"',
+            'data-update'=>'data-update="content_schedab_arbustive"'
+        );
+        $actions['first']=array(
+            'url'=>'href="?'.$baseurl.'&start=0"',
+            'data-update'=>'data-update="content_schedab_arbustive"'
+        );
+    }
+
+    if ($start<$countall-$items_in_page) {
+
+        $actions['next']=array(
+            'url'=>'href="?'.$baseurl.'&start='.min($start+$items_in_page,$last_page).'"',
+            'data-update'=>'data-update="content_schedab_arbustive"'
+        );
+         $actions['last']=array(
+            'url'=>'href="?'.$baseurl.'&start='.$last_page .'"',
+            'data-update'=>'data-update="content_schedab_arbustive"'
+        );
+    }
+    ?>
+    <div class="scrollcontrols">
+        <span>
+        <a <?php echo $actions['first']['url'];?> <?php echo $actions['first']['data-update'];?> >
+            <img class="actions first" src="images/empty.png" title="Primo">
+        </a>
+        <a <?php echo $actions['prev']['url'];?> <?php echo $actions['prev']['data-update'];?> >
+            <img class="actions prev" src="images/empty.png" title="Precedente">
+        </a>
+        </span>
+        <span>
+            Specie <input id="current" name="current" value="<?php echo $start; ?>" type="text"  /> di <?php echo $countall; ?></span>
+        <a href="#" style="display: none;" id="confirm_move">
+            <img class="actions confirm" src="images/empty.png" title="Vai">
+        </a>
+        <a href="#" style="display: none;" id="cancel_move">
+            <img class="actions cancel" src="images/empty.png" title="Annulla">
+        </a>
+        </span>
+        <span>
+        <a <?php echo $actions['next']['url'];?> <?php echo $actions['next']['data-update'];?> >
+            <img class="actions next" src="images/empty.png" title="Successivo">
+        </a>
+        <a <?php echo $actions['last']['url'];?> <?php echo $actions['last']['data-update'];?> >
+            <img class="actions last" src="images/empty.png" title="Ultimo">
+        </a>
+        </span>
+    </div>
+</div>
+    
