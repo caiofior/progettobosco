1a0fb37bf38801c93727f6a9fd968785f4cd556d
Claudio Fior CRA <caiofior@gmail.com>
2013-05-06 15:18:11 +0200

    Fixed contacts

--- views/blocks/general/footer.php
+++ views/blocks/general/footer.php
@@ -24,10 +24,10 @@
 			
 			<div class="footer-list">
 				<ul>			
-                                        <li><a href="http://sito.entecra.it/	"><img src="images/cra.jpg" alt="Logo CRA"/></a></li>
-					<li><a href="http://mpf.entecra.it/">CRA-MPF</a></li>
-					<li><a href="http://www.selvicoltura.eu/">CRA-ISS</a></li>
-					<li><a href="https://github.com/caiofior/progettobosco">GitHub</a></li>
+                                        <li><a rel="nofollow" href="http://sito.entecra.it/	"><img src="images/cra.jpg" alt="Logo CRA"/></a></li>
+					<li><a rel="nofollow" href="http://mpf.entecra.it/">CRA-MPF</a></li>
+					<li><a rel="nofollow" href="http://www.selvicoltura.eu/">CRA-ISS</a></li>
+					<li><a rel="nofollow" href="https://github.com/caiofior/progettobosco">GitHub</a></li>
 				</ul>
 			</div>					
 				
@@ -41,7 +41,7 @@
 			
 			<div class="footer-list">
                 <ul>
-                        <li><a href="http://www.progettobosco.it/" title="Progettobosco">Progettobosco</a></li>
+                        <li><a rel="nofollow" href="http://www.progettobosco.it/" title="Progettobosco">Progettobosco</a></li>
 			
 				</ul>
 			</div>			
@@ -53,8 +53,9 @@
 			<h3>Dubbi e suggerimenti</h3>			
 			
 			<p>
-			<a href="index.html"><img src="https://secure.gravatar.com/avatar/2e8d2d37da66c6874a65f69879f8e590?s=140&amp;d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png" width="40" height="40" alt="Programmazione e sviluppo" class="float-left" /></a>
-			Per dubbi e suggerimenti puoi contattare chi ha realizzato queste pagine su  <a href="https://github.com/caiofior">Github</a></p>			
+			<a href="<?php echo $GLOBALS['BASE_URL'];?>"><img src="https://secure.gravatar.com/avatar/2e8d2d37da66c6874a65f69879f8e590?s=140&amp;d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png" width="40" height="40" alt="Programmazione e sviluppo" class="float-left" /></a>
+			Per dubbi e suggerimenti puoi contattare chi ha realizzato queste pagine su  </p>
+                        <a rel="nofollow" href="https://www.facebook.com/pages/Forest-Management/516686821727689" title="Facebook"><img src="images/facebook.png" alt="Facebook"></a>
 			
 		</div>		
 		
@@ -64,13 +65,13 @@
 		<div id="footer-bottom">
 	
 			<div class="bottom-left">
-<a rel="license" href="http://creativecommons.org/licenses/by/3.0/it/"><img alt="Licenza Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/it/88x31.png" /></a><br />Quest' opera è distribuita con <a rel="license" href="http://creativecommons.org/licenses/by/3.0/it/">licenza Creative Commons Attribuzione 3.0 Italia</a>.			</div>		
+<a rel="nofollow" href="http://creativecommons.org/licenses/by/3.0/it/"><img alt="Licenza Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/it/88x31.png" /></a><br />Quest' opera è distribuita con <a rel="license" href="http://creativecommons.org/licenses/by/3.0/it/">licenza Creative Commons Attribuzione 3.0 Italia</a>.			</div>		
 	
 			<div class="bottom-right">
 				<p>		
-					<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | 
+					<a rel="nofollow" href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | 
 		   		<a href="http://validator.w3.org/check/referer">XHTML</a>	|			
-					<a href="<?php echo $GLOBALS['BASE_URL'];?>">Home</a> |
+					<a rel="nofollow" href="<?php echo $GLOBALS['BASE_URL'];?>">Home</a> |
 					<strong><a href="#top" class="back-to-top">Vai all'inizio</a></strong>								
 				</p>
 			</div>
