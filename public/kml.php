8e76db400dd91a4712fc159ab9e14bc26981cf17
Claudio Fior CRA <caiofior@gmail.com>
2013-06-05 17:00:21 +0200

    Fixed map update

--- public/kml.php
+++ public/kml.php
@@ -15,14 +15,16 @@ $poligon->loadFromId($_REQUEST['id_av']); ?>
 <Placemark id="<?php echo $poligon->getData('id_av');?>">
 	<Style><LineStyle><color>ff0000ff</color></LineStyle><PolyStyle><fill>0</fill></PolyStyle></Style>
       <Polygon><outerBoundaryIs><LinearRing><coordinates><?php
-foreach($poligon->getVertexColl() as $key=>$poligonitem) {
+foreach($poligon->getSemplifiedVertexColl() as $key=>$poligonitem) {
   if ($key!= 0) echo ' ';
   echo $poligonitem->getRawData('longitude').','.$poligonitem->getRawData('latitude');
 }?></coordinates></LinearRing></outerBoundaryIs></Polygon>
 </Placemark>
-<?php elseif (key_exists('forest_id', $_REQUEST)) :
+<?php elseif (key_exists('forest_id', $_REQUEST)) : 
 $forest = new \forest\Forest();
 $forest->loadFromCode($_REQUEST['forest_id']);
+?>
+<?php 
 $poligoncoll = $forest->getPoligonColl();
 $poligoncoll->loadAll();
 foreach($poligoncoll->getItems() as $poligon) : ?>
@@ -33,8 +35,7 @@ foreach($poligon->getSemplifiedVertexColl() as $key=>$poligonitem) {
   if ($key!= 0) echo ' ';
   echo $poligonitem->getRawData('longitude').','.$poligonitem->getRawData('latitude');
 }?></coordinates></LinearRing></outerBoundaryIs></Polygon>
-</Placemark>  
-<?php 
-endforeach;
-endif;?>
+</Placemark>
+<?php endforeach; ?>
+<?php endif;?>
 </kml>
