0f844ef3c4fb63e5eaa671e91f81ec7f95ddacd1
Claudio Fior CRA <caiofior@gmail.com>
2013-06-20 17:01:29 +0200

    Terrain assets added to forest compartment description

--- lib/pb/forest/mediator/a_description.php
+++ lib/pb/forest/mediator/a_description.php
@@ -41,6 +41,11 @@ class ADescription  {
      */
     private $issues=array();
     /**
+     * Text lines composing the description
+     * @var array
+     */
+    private $t=array();
+    /**
      * Intantiates the Decsription
      * @param \forest\entity\A $a
      */
@@ -53,30 +58,76 @@ class ADescription  {
      */
     public function generateDescription() {
         $description = '';
-        $issues =array();
         if (!$this->a instanceof \forest\entity\A )
             return $description;
         
-        $pf1 = $this->getValue('a','pf1', $this->a->getData('pf1'));
-        if ($pf1 != '') $pf1 = 'Posta '.$pf1.$this->getNote('a', 'pf1');
+        $this->t['a']['pf1'] = $this->getValue('a','pf1', $this->a->getData('pf1'));
+        if ($this->t['a']['pf1'] != '') $this->t['a']['pf1'] = 'Posta '.$this->t['a']['pf1'].$this->getNote('a', 'pf1');
         else $this->addIssue('a', 'pf1', 'Posizione fisiografica non inserita');
         
-        $ap = $this->a->getData('ap');
-        if ($ap != '') $ap = ' ad un\'altitudine prevalente di '.$ap. ' metri'.$this->getNote('a', 'ap');
+        $this->t['a']['ap'] = $this->a->getData('ap');
+        if ($this->t['a']['ap'] != '') $this->t['a']['ap'] = ' ad un\'altitudine prevalente di '.$this->t['a']['ap']. ' metri'.$this->getNote('a', 'ap');
         else $this->addIssue('a', 'pf1','Altitudine prevalente non inserita');
         
-        $e1 = $this->getValue('a','e1', $this->a->getData('e1'));
-        if ($e1 != '') $e1 = ' esposizione prevalente '.$e1.$this->getNote('a', 'e1');
+        $this->t['a']['e1'] = $this->getValue('a','e1', $this->a->getData('e1'));
+        if ($this->t['a']['e1'] != '') $this->t['a']['e1'] = ' esposizione prevalente '.$this->t['a']['e1'].$this->getNote('a', 'e1');
         else $this->addIssue('a', 'e1','Esposizione prevalente non inserita');
         
-        $pp = $this->getValue('a','pp', $this->a->getData('pp'));
-        if ($pp != '') $pp = ' pendenza prevalente '.$e1.$this->getNote('a', 'pp');
+        $this->t['a']['pp'] = $this->a->getData('pp');
+        if ($this->t['a']['pp'] != '') $this->t['a']['pp'] = ' pendenza prevalente del '.$this->t['a']['pp'].' % '.$this->getNote('a', 'pp');
         else $this->addIssue('a', 'pp','Pendenza prevalente non inserita');
+        
+        $this->t['a']['o'] = '';
+        switch ($this->a->getData('o')) {
+           case 1 :
+                $this->t['a']['o'] = ', accidentalità debole'.$this->getNote('a', 'o');
+           break;
+           case 2 :
+                $this->t['a']['o'] = ', accidentalità media'.$this->getNote('a', 'o');
+           break;
+           case 3:
+                $this->t['a']['o'] = ', accidentalità forte'.$this->getNote('a', 'o');
+           break;
+           case 4:
+                $this->t['a']['o'] = ', accidentalità molto forte'.$this->getNote('a', 'o');
+           break;
+           default:
+               $this->addIssue('a', 'o','Ostacoli agli interventi non inseriti');
+           break;
+        }
+        
+        
+        $terrain_labels = array(
+               1 => ' pericolo di peggioramento della situazione di dissesto causato da',
+               2 => ' alcuni contenuti problemi di dissesto legati alla presenza di',
+               3 => ' grave dissesto dovuto a',
+               4 => ' dissesto molto grave imputabile a'
+        );
+        $terrain_assets = array(
+               2 => ' erosione superficiale - incanalata',
+               3 => ' erosione catastrofica - calanchiva',
+               4 => ' frane superficiali',
+               5 => ' frane di profondità - crolli',
+               6 => ' rotolamento massi',
+               7 => ' '.$this->a->getData('a8')
+        );
+        foreach ($terrain_assets as $key => $terrain_asset) {
+            if (key_exists($this->a->getData('a'.$key),$terrain_labels)) {
+                if ($this->t['a']['a'.$key] == '')
+                    $this->t['a']['a'.$key] = $terrain_labels[$this->a->getData('a'.$key)];
+                $this->t['a']['a'.$key] .= $terrain_asset[$key].', ';
+            }
+            else
+                $this->t['a']['a'.$key] = '';
+        }
+        $this->t['a']['a'.$key] .= $this->getNote('a', 'a2');
+        
         
         $description .= <<<EOT
 Particella {$this->a->getData('proprieta')} {$this->a->getData('toponimo')} {$this->a->getData('sup_tot')} ha
 Fattori ambientali e di gestione
-{$pf1}{$ap}{$e1}{$pp}
+{$this->t['a']['pf1']}{$this->t['a']['ap']}{$this->t['a']['e1']}{$this->t['a']['pp']}{$this->t['a']['o']}
+{$this->t['a']['a2']}{$this->t['a']['a3']}{$this->t['a']['a4']}{$this->t['a']['a5']}{$this->t['a']['a6']}{$this->t['a']['a7']}
 EOT;
                 
         
