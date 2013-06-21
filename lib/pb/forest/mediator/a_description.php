f1a1876e4b580bbaefc790d6b2fba56de2cfae01
Claudio Fior CRA <caiofior@gmail.com>
2013-06-21 16:55:57 +0200

    Progress on form a description

--- lib/pb/forest/mediator/a_description.php
+++ lib/pb/forest/mediator/a_description.php
@@ -111,23 +111,141 @@ class ADescription  {
                6 => ' rotolamento massi',
                7 => ' '.$this->a->getData('a8')
         );
+        $this->t['a']['a'] = '';
         foreach ($terrain_assets as $key => $terrain_asset) {
             if (key_exists($this->a->getData('a'.$key),$terrain_labels)) {
-                if ($this->t['a']['a'.$key] == '')
-                    $this->t['a']['a'.$key] = $terrain_labels[$this->a->getData('a'.$key)];
-                $this->t['a']['a'.$key] .= $terrain_asset[$key].', ';
+                $this->t['a']['a'.$key] .= $terrain_labels[$this->a->getData('a'.$key)];
+                $this->t['a']['a'.$key] .= $terrain_asset.', ';
             }
-            else
-                $this->t['a']['a'.$key] = '';
         }
-        $this->t['a']['a'.$key] .= $this->getNote('a', 'a2');
+        $this->t['a']['a'] .= $this->getNote('a', 'a2');
+        
+        $water_remain_labels = array(
+                  1 => ' su meno del 30% della superficie',
+                  2 => ' su di una superficie compresa fra il 30 e il 60% del totale',
+                  3 => ' su più del 60% della superficie'
+        );
+        $water_remain_assets = array(
+                  2 => ' terreno superficiale',
+                  3 => ' rocce affioranti',
+                  4 => ' pietrosità',
+                  5 => ' ristagni d\'acqua'
+        );
+        $this->t['a']['r'] = '';
+        foreach ($water_remain_assets as $key => $water_asset) {
+            if (key_exists($this->a->getData('r'.$key),$water_remain_labels)) {
+                if ($this->t['a']['r'] == '')
+                    $this->t['a']['r'] = 'Possibili limitazioni allo sviluppo dell\'apparato radicale per la presenza di ';
+                $this->t['a']['r'] .= $water_asset.$water_remain_labels[$this->a->getData('r'.$key)].', ';
+            }
+        }
+        
+        $phitosanitary_labels = array(
+            1 => ' pericolo di peggioramento della situazione fitosanitaria dovuto a',
+            2 => ' danni lievi causati da',
+            3 => 'danni gravi causati da',
+            4 => ' danni molto gravi causati da'
+        );
+        $phitosanitary_assets = array(
+           2 => ' sovrapascolamento',
+           3 => ' selvatici',
+           4 => ' agenti fitopatogeni e parassiti',
+           5 => ' agenti meteorici',
+           6 => ' movimenti di neve',
+           7 => ' incendio',
+           8 => ' utilizzazioni',
+           9 => ' movimenti di terra',
+          10 => ' attività turistico-ricreative',
+          11 =>''
+        );
+        $this->t['a']['f'] = '';
+        foreach ($phitosanitary_assets as $key => $phitosanitary_asset) {
+            if (key_exists($this->a->getData('f'.$key),$phitosanitary_labels)) {
+                $this->t['a']['f'] .= $phitosanitary_asset.$phitosanitary_labels[$this->a->getData('f'.$key)].', ';
+            }
+        }
+        $this->t['a']['f'] .= $this->getNote('a', 'f2');
+        
+        $particular_assets = array(
+               2 => ' pascolo in bosco',
+               3 => ' emergenze storico-naturalistiche',
+               4 => ' sorgenti, fonti',
+               5 => ' sottoposta a usi civici',
+               6 =>''
+        );
+        $this->t['a']['p'] = '';
+        foreach ($particular_assets as $key => $particular_asset) {
+            if ($key == 2) {
+                switch ($this->a->getData('p'.$key)) {
+                     case 1 :
+                          $this->t['a']['p'] .= $particular_asset.' di bovini';
+                     break;
+                     case 2 :
+                          $this->t['a']['p'] .= $particular_asset.' di ovini';
+                     break;
+                     case 3:
+                          $this->t['a']['p'] .= $particular_asset.' di equini';
+                     break;
+                     case 4:
+                          $this->t['a']['p'] .= $particular_asset.' di caprini';
+                     break;
+                     case 5:
+                          $this->t['a']['p'] .= $particular_asset.$this->a->getData('p9');
+                     break;
+                }
+            }
+            else if ($this->a->getData('p'.$key) == 1) {
+                $this->t['a']['p'] .= $particular_asset.', ';
+            }
+        }
+        if ($this->t['a']['p'] == '')
+            $this->t['a']['p'] = 'Assenza di fatti particlolari';
+        else {
+            $this->t['a']['p'] = 'Fatti particlolari: '.$this->t['a']['p'];
+            $this->t['a']['p'] .= $this->getNote('a', 'p2');
+        }
+        
+
+        $manufacts_assets = array(
+            2  => ' strade camionabili',
+            3  => ' strade trattorabili',
+            4  => ' piste forestali',
+            5  => ' edifici',
+            6  => ' sistemazioni',
+            7  => ' gradonamenti',
+            8  => ' muri - recinzioni',
+            9  => ' paravalanghe',
+            10 => ' elettrodotti',
+            11 => ' cesse parafuoco',
+            12 => ' tracciati teleferiche',
+            13 => ' condotte idriche',
+            14 => ' parcheggi',
+            15 => ' cave',
+            16 => ' sentieri guidati',
+            17 => ' piste-impianti sciistici',
+            18 => '',
+            20 => ' piazzali-buche di carico',
+            21 => ' strade camionabili',
+            22 => ' tracciati per mezzi agricoli minori',
+            23 => ' aree sosta'
+        );
+        $this->t['a']['m'] = '';
+        foreach ($manufacts_assets as $key => $manufacts_asset) {
+            if (key_exists($this->a->getData('m'.$key),$phitosanitary_labels)) {
+                $this->t['a']['m'] .= $manufacts_asset.', ';
+            }
+        }
+        if ($this->t['a']['m'] != '') {
+            $this->t['a']['m'] = 'Presenti '.$this->t['a']['m'];
+            $this->t['a']['m'] .= $this->getNote('a', 'm2');
+        }
         
         
         $description .= <<<EOT
 Particella {$this->a->getData('proprieta')} {$this->a->getData('toponimo')} {$this->a->getData('sup_tot')} ha
 Fattori ambientali e di gestione
 {$this->t['a']['pf1']}{$this->t['a']['ap']}{$this->t['a']['e1']}{$this->t['a']['pp']}{$this->t['a']['o']}
-{$this->t['a']['a2']}{$this->t['a']['a3']}{$this->t['a']['a4']}{$this->t['a']['a5']}{$this->t['a']['a6']}{$this->t['a']['a7']}
+{$this->t['a']['a']}{$this->t['a']['r']}{$this->t['a']['f']}{$this->t['a']['p']}{$this->t['a']['m']}
 EOT;
                 
         
